778f79ed7e8e0ba5aaa512241992e9994cd1541e
staszewski <kamilstaszewski95@gmail.com>
2021-03-28 08:29:56 +0200

    Init setup

---
+++ apps/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+import { BASE_URL } from "@/consts";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  data = {};
+  mounted() {
+    axios.get(`${BASE_URL}`).then((response) => {
+      return (this.data = response.data);
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ apps/frontend/src/consts.ts
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000/api"
+    : "https://url-analyzer.vercel.app/api";
