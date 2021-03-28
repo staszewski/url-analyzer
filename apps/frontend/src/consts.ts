c79fa3b21f4529cc7e7b0255228b0f92594f7a1a
staszewski <kamilstaszewski95@gmail.com>
2021-03-28 10:30:27 +0200

    Update const

--- apps/frontend/src/consts.ts
+++ apps/frontend/src/consts.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://url-analyzer.vercel.app/api";
+    : "https://url-analyzer-frontend.vercel.app";
