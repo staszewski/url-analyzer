486d798989c8fe322fae8f515276791154876e80
staszewski <kamilstaszewski95@gmail.com>
2021-03-28 10:34:42 +0200

    Correct link

--- apps/frontend/src/consts.ts
+++ apps/frontend/src/consts.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://url-analyzer-frontend.vercel.app";
+    : "https://url-analyzer-backend.vercel.app/api";
